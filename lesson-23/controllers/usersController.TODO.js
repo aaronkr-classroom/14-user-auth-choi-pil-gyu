516d521ffd64d3db93d109e50fe0451b361c9fd9
choi-pil-gyu <162441357+choi-pil-gyu@users.noreply.github.com>
2024-06-12 02:58:04 +0000

    12주차 끝 2

--- lesson-23/controllers/usersController.TODO.js
+++ lesson-23/controllers/usersController.TODO.js
@@ -1,6 +1,7 @@
 // controllers/usersController.js
 "use strict";
 
+const { validate } = require("../models/Talk");
 /**
  * Listing 18.11 (p. 271)
  * userController.js에서 인덱스 액션 생성과 index 액션의 재방문
@@ -31,10 +32,42 @@ module.exports = {
    * Listing 23.3 (p. 336)
    * userController.js로의 로그인과 인증 액션 추가
    */
+  login: (req, res) => {
+    res.render("users/login", {
+      page: "login",
+      title: "Login"
+    });
+  },
+  
+  authenticate: (req, res, next) => {
+    User.findOne({ email: req.body.email})
+      .then(user => {
+        if (user) {
+          user.passwordCompare(req.body.password)
+            .then(pwMatch => {
+              if (pwMatch) {
+                res.locals.redirect = "/users/${user._id}";
+                req.flash(
+                  "error",
+                  "Login failed"
+                }
+            })
 
-  /**
-   * @TODO: authenticate 액션
-   */
+        } else {
+          res.locals.redirect = "/users/login";
+          req.flash(
+            "error",
+            "Login failed"
+          );
+          next();
+        })
+
+      })
+      .catch(error => {
+        console.log(`error logging in: ${error.message}`);
+        next(error);
+      })
+  }
 
   index: (req, res, next) => {
     User.find() // index 액션에서만 퀴리 실행
@@ -115,7 +148,35 @@ module.exports = {
    * Listing 23.7 (p. 346)
    * userController.js에서 validate 액션 추가
    */
+  validate: (req, res, next) => {
+    req
+      .sanitizeBody("email")
+      .normalizeEmail({
+        all_lowercase: true
+      })
+      .trim()
+      .check("email", "Email is invalid")
+      .isEmail();
 
+    req
+      .check("password", "Password cannot be empty")
+      .notEmpty();
+
+    req.getValidationResult()
+      .then(result => {
+        if (!result.isEmpty()) {
+          let messages = result.array().map(m => m.msg);
+          req.skip = true;
+          req.flash("error", messages.join( "and"));
+          res.locals.redirect = "/users/n"
+        }
+
+      })
+      .catch(error => {
+        console.log(`Error: ${error.message}`);
+        next(error);
+      });
+  },
   /**
    * [노트] 폼 데이터를 다시 채우기 위해 다양한 방법을 선택할 수 있다. (연구해보면)
    * 어떤 패키지가 효과적인지 알게 될 것이다. 자신에게 가장 적합한 방법을 찾으면
--- lesson-24/app.js
+++ lesson-24/app.js
@@ -32,6 +32,11 @@ router.use(
 ); // method-override 미들웨어를 사용
 
 /**
+ * =====================================================================
+ * Flash Messages and Session
+ * =====================================================================
+ */
+/**
  * Listing 22.1 (p. 325)
  * app.js에서의 플래시 메시지 요청
  */
@@ -55,12 +60,42 @@ router.use(
 router.use(connectFlash()); // connect-flash 미들웨어를 사용
 
 /**
+ * =====================================================================
+ * Passport Configuration and Middleware
+ * =====================================================================
+ */
+/**
+ * @TODO: 
+ * 
+ * Listing 24.1 (p. 351)
+ * main.js에서 passport의 요청과 초기화
+ */
+const passport = require("passport");
+router.use(passport.initialize());// passport를 요청
+router.use(passport.session());// passport를 초기화
+// passport가 Express.js 내 세션을 사용하도록 설정
+
+/**
+ * @TODO: 
+ * 
+ * Listing 24.2 (p. 351)
+ * main.js에서 passport 직렬화 설정
+ */
+const User = require("./models/User");
+passport.use(User.createStrategy());// User 모델의 인증 전략을 passport에 전달
+passport.serializeUser(User.serializeUser());// User 모델의 직렬화 메서드를 passport에 전달
+passport.deserializeUser(User.deserializeUser());// User 모델의 역직렬화 메서드를 passport에 전달
+
+/**
  * Listing 22.2 (p. 327)
  * 응답상에서 connectFlash와 미들웨어와의 연계
  */
 router.use((req, res, next) => {
   // 응답 객체상에서 플래시 메시지의 로컬 flashMessages로의 할당
   res.locals.flashMessages = req.flash(); // flash 메시지를 뷰에서 사용할 수 있도록 설정
+
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentUser = req.user;
   next();
 });
 
@@ -81,7 +116,6 @@ const db = mongoose.connection;
 db.once("open", () => {
   console.log("db is connect!");
 });
-
 /**
  * =====================================================================
  * Define app settings and middleware
@@ -116,11 +150,20 @@ router.get("/about", pagesController.showAbout); // 코스 페이지 위한 라�
 router.get("/transportation", pagesController.showTransportation); // 교통수단 페이지 위한 라우트 추가
 
 /**
- * @TODO: login 라우트 추가
- *
  * Listing 23.2 (p. 335)
  * app.js로 로그인 라우트를 추가
  */
+router.get("/users/login", usersController.login); // 로그인 폼을 보기 위한 요청 처리
+router.post(
+  "/users/login",
+  usersController.authenticate,
+  usersController.redirectView
+); // 로그인 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
+
+// @TODO: 
+
+// Listing 24.7 후에 (p. 358)
+// 로그아웃을 위한 라우트 추가
 
 /**
  * Users
